1969eb35aa458451a975c2d17c0c8b3b44150b80
Viola Shephard <shephardviola@gmail.com>
2025-03-30 18:26:47 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+/* --- FUNCTION FOR THE CLOCK --- */
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function checkDay() {
+    var today = new Date()
+    var day = today.getDate()
+    var month = today.getMonth() + 1
+    var year = today.getFullYear()
+    $('#date').html(year + '-' + month + '-' + day)
+}
+
+
+
